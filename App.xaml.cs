0fd77bbd952e261d888d202bd8c2cf699e7a7283
dell <dell@DESKTOP-RFM088N>
2024-12-17 15:44:00 +0545

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SpendWise
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
